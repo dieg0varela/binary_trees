1ca9c1293fd946d0f5c9010728881d286d33e705
Ro8s <rodrigo.delgado3132@gmail.com>
2021-07-19 17:26:17 +0000

    added non-functional ex 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -12,5 +12,27 @@
 */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	
+	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+	int val = 0;
+
+	if (!parent)
+		return (NULL);
+	if (!new_node)
+		return (NULL);
+	if (parent->left)
+	{
+		val = parent->left->n;
+		parent->left = NULL;
+		binary_tree_insert_left(parent, value);
+		binary_tree_insert_left(parent->left, val);
+	}
+	else
+	{
+		new_node->n = value;
+		new_node->left = NULL;
+		new_node->right = NULL;
+		new_node->parent = parent;
+		parent->left = new_node;
+	}
+	return (new_node);
 }
