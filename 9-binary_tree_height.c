9283f6082bc56c9bb5e228896e03cc77dab1d9d6
Diego Varela <2806@holbertonschool.com>
2021-07-19 16:45:36 -0300

    Fix height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -6,14 +6,15 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t left_side;
+	size_t right_side;
+	
 	if(tree == NULL)
 		return (0);
 	else
 	{
-		size_t left_side;
-		size_t right_side;
-		left_side = height_of_binary_tree(tree->left);
-		right_side = height_of_binary_tree(tree->right);
+		left_side = binary_tree_height(tree->left);
+		right_side = binary_tree_height(tree->right);
 		if(left_side > right_side)
 			return left_side + 1;
 		else
