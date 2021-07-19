43b087d4492a52574e594149e87ecbdc5e55137a
Diego Varela <2806@holbertonschool.com>
2021-07-19 15:58:52 -0300

    Upload changes in is_root and preorder

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -2,13 +2,13 @@
 /**
 * binary_tree_is_root - check if a node is a root
 * @node: the node to check
-* 
+*
 * Return: 1 if a node is a root, otherwise retuns 0
 */
 int binary_tree_is_root(const binary_tree_t *node)
 {
-	if(!node)
-		return (0);    
+	if (!node)
+		return (0);
 	if (!(node->parent))
 		return (1);
 	return (0);
--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 /**
-* binary_tree_preorder - function that goes through a binary tree using pre-order traversal
+* binary_tree_preorder - f. that goes in a binary tree using pre-order traversal
 * @tree: Pointer to the parent node
 * @func: Pointer to a function to call for each node
 * Return: void.
@@ -9,9 +9,9 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (!tree || !func)
 		return;
+	func(tree->n);
 	if (tree->left)
 		binary_tree_preorder(tree->left, func);
 	if (tree->right)
 		binary_tree_preorder(tree->right, func);
-	func(tree->n);
 }
