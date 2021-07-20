3abea18a518aa6036b735c77ad871b9bbf07c95b
Diego Varela <2806@holbertonschool.com>
2021-07-20 14:16:53 -0300

    Fix betty task height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 /**
- * tree_height - func. that gets the height of the tree 
+ * tree_height - func. that gets the height of the tree
  * @tree: Pointer to the tree
  * @level: level of the tree
  * @height: Pointer to height integer
@@ -8,21 +8,20 @@
  */
 void tree_height(const binary_tree_t *tree, size_t level, size_t *height)
 {
-	if(tree == NULL)
+	if (tree == NULL)
 		return;
-	else
+
+	if (level > *height)
 	{
-		if (level > *height)
-		{
-			*height = *height + 1;
-		}
-		tree_height(tree->left, level + 1, height);
-		tree_height(tree->right, level + 1, height);
+		*height = *height + 1;
 	}
+	tree_height(tree->left, level + 1, height);
+	tree_height(tree->right, level + 1, height);
+
 }
 
 /**
- * binary_tree_height - func. that gets the height of the tree 
+ * binary_tree_height - func. that gets the height of the tree
  * @tree: Pointer to the tree
  * Return: the height of the tree
  */
@@ -30,7 +29,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t pichu = 0;
 
-	if(tree == NULL)
+	if (tree == NULL)
 		return (0);
 	tree_height(tree, 0, &pichu);
 
