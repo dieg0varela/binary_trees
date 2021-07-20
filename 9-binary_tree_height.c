892960a32922a4b98aa77fff67ccb4a529b2bce4
Diego Varela <2806@holbertonschool.com>
2021-07-20 14:01:17 -0300

    Upload height working

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,23 +1,33 @@
 #include "binary_trees.h"
 /**
+ * tree_height - func. that gets the height of the tree 
+ * @tree: Pointer to the tree
+ * @level: level of the tree
+ * @height: Pointer to height integer
+ * Return: the height of the tree
+ */
+void tree_height(const binary_tree_t *tree, size_t level, size_t *height)
+{
+	if (level > *height)
+	{
+		*height = *height + 1;
+	}
+	tree_height(tree->left, level + 1, height);
+	tree_height(tree->right, level + 1, height);
+}
+
+/**
  * binary_tree_height - func. that gets the height of the tree 
  * @tree: Pointer to the tree
  * Return: the height of the tree
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_side;
-	size_t right_side;
-	
+	size_t pichu = 0;
+
 	if(tree == NULL)
 		return (0);
-	else
-	{
-		left_side = binary_tree_height(tree->left);
-		right_side = binary_tree_height(tree->right);
-		if(left_side > right_side)
-			return left_side + 1;
-		else
-			return right_side + 1;
-	}
+	tree_height(tree, 0, &pichu);
+
+	return (pichu);
 }
