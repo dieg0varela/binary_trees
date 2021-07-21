543f9806a021cd38f27499f94aa9d8d9f9ba7d4e
Diego Varela <2806@holbertonschool.com>
2021-07-21 11:58:00 -0300

    Fix betty task 8

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 /**
-* binary_tree_postorder -f. that goes in a binary tree using postorder traversal
+* binary_tree_postorder -f.that goes in a binary tree using postorder traversal
 * @tree: Pointer to the parent node
 * @func: Pointer to a function to call for each node
 * Return: void.
