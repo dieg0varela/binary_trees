d47a3a3a61b9599810a0920919847ba4444085f9
Diego Varela <2806@holbertonschool.com>
2021-07-22 10:57:05 -0300

    Fix tree balance

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,5 +1,40 @@
 #include "binary_trees.h"
-#include "9-binary_tree_height.c"
+/**
+ * tree_height - func. that gets the height of the tree
+ * @tree: Pointer to the tree
+ * @level: level of the tree
+ * @height: Pointer to height integer
+ * Return: the height of the tree
+ */
+void tree_height(const binary_tree_t *tree, int level, int *height)
+{
+	if (tree == NULL)
+		return;
+
+	if (level > *height)
+	{
+		*height = *height + 1;
+	}
+	tree_height(tree->left, level + 1, height);
+	tree_height(tree->right, level + 1, height);
+
+}
+
+/**
+ * binary_tree_height - func. that gets the height of the tree
+ * @tree: Pointer to the tree
+ * Return: the height of the tree
+ */
+int bin_tree_height(const binary_tree_t *tree)
+{
+	int pichu = 1;
+
+	if (tree == NULL)
+		return (0);
+	tree_height(tree, 0, &pichu);
+
+	return (pichu);
+}
 /**
  * binary_tree_balance - enitre balance of a binary tree
  * @tree: Pointer of the tree
@@ -10,10 +45,13 @@ int binary_tree_balance(const binary_tree_t *tree)
 	int izq = 0;
 	int der = 0;
 
-	if (tree == NULL)
+	if (!tree)
 		return (0);
-	izq = (int)binary_tree_height(tree->left);
-	der = (int)binary_tree_height(tree->right);
+	if (!(tree->left) && !(tree->right))
+		return (0);
+
+	izq = bin_tree_height(tree->left);
+	der = bin_tree_height(tree->right);
 	return (izq - der);
 
 }
