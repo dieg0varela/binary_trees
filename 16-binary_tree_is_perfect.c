9f1698d576c79cad1c0749f4bcb8d73c8120bc31
Diego Varela <2806@holbertonschool.com>
2021-07-22 11:26:45 -0300

    Fix is perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,6 +1,7 @@
 #include "binary_trees.h"
-#include "15-binary_tree_is_full.c"
-#include "14-binary_tree_balance.c"
+#include "math.h"
+#include "9-binary_tree_height.c"
+#include "11-binary_tree_size.c"
 /**
  * binary_tree_is_perfect - Entry point
  * @tree:Point to the tree
@@ -8,15 +9,14 @@
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	 int balance = 0;
-	 int full = 0;
+	int height = 0;
+	int size = 0;
 
 	if (!tree)
 		return (0);
-
-	balance = binary_tree_balance(tree);
-	full = binary_tree_is_full(tree);
-	if (balance == 0 && full == 1)
+	height = binary_tree_height(tree) + 1;
+	size = binary_tree_size(tree);
+	if (pow(2, height) == size + 1)
 		return (1);
 	return (0);
 
