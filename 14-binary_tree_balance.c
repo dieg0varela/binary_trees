7d9010a592dc395d4469c0a47c435b8cac8887b7
Diego Varela <2806@holbertonschool.com>
2021-07-22 10:07:16 -0300

    Fix task Balance

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,36 +1,5 @@
 #include "binary_trees.h"
-
-/**
- * count_left - func. that count the balance of the left tree
- * @tree: Pointer to the tree
- * @izq: Pointer to count of the left nodes
- * Return: the size of the tree
- */
-void count_left(const binary_tree_t *tree, int *izq)
-{
-	if (!tree)
-		return;
-
-	count_left(tree->left, izq);
-	*izq = *izq + 1;
-
-}
-/**
- * count_right - func. that count the right nodes of the tree
- * @tree: Pointer to the tree
- * @der: Pointer to right count integer
- * Return: the size of the tree
- */
-void count_right(const binary_tree_t *tree, int *der)
-{
-	if (!tree)
-		return;
-
-	count_right(tree->right, der);
-	*der = *der + 1;
-
-}
-
+#include "9-binary_tree_height.c"
 /**
  * binary_tree_balance - enitre balance of a binary tree
  * @tree: Pointer of the tree
@@ -38,13 +7,13 @@ void count_right(const binary_tree_t *tree, int *der)
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int izq = 0;
-	int der = 0;
+	size_t izq = 0;
+	size_t der = 0;
 
 	if (tree == NULL)
 		return (0);
-	count_left(tree, &izq);
-	count_right(tree, &der);
-	return (izq - der);
+	izq = binary_tree_height(tree->left);
+	der = binary_tree_height(tree->right);
+	return ((int)izq - (int)der);
 
 }
